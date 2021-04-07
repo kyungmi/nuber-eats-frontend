d67ea74e318c0723c8b20c2e277ced2c0064e191
Kyungmi Koong <kyungmi.k@woowahan.com>
2021-04-07 19:57:57 +0900

    14.4 React Router Dom

---
+++ src/routers/logged-in-router.tsx
@@ -0,0 +1,3 @@
+import React, { FC } from 'react';
+
+export const LoggedInRouter: FC = () => <span>Logged In</span>;
---
+++ src/routers/logged-out-router.tsx
@@ -0,0 +1,3 @@
+import React, { FC } from 'react';
+
+export const LoggedOutRouter: FC = () => <span>Logged Out</span>;
