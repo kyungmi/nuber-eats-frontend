855999c018a16e8edb95d8ec5de19fb764ea11a9
Kyungmi Koong <kyungmi.k@woowahan.com>
2021-04-10 13:43:17 +0900

    15.1 React Hook Form

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import { client } from './apollo';
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-     <App />
+      <App />
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,14 +1,44 @@
 import React, { FC } from 'react';
+import { useForm } from 'react-hook-form';
 import { isLoggedInVar } from '../apollo';
 
 export const LoggedOutRouter: FC = () => {
-  const onClick = () => {
-    isLoggedInVar(true);
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = () => {
+    console.log(watch('email'));
   };
+  const onInvalid = (invalid: any) => {
+    console.log(errors);
+  };
+
   return (
     <div>
       <h1>Logged Out</h1>
-      <button onClick={onClick}>Click to login</button>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            type="email"
+            placeholder="email"
+            {...register('email', {
+              required: 'This is required',
+              pattern: /^[A-Za-z0-9._%+-]+@gamil.com$/,
+            })}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="password"
+            {...register('password', { required: true })}
+          />
+        </div>
+        <button className="bg-yellow-300 text-white">Submit</button>
+      </form>
     </div>
   );
 };
