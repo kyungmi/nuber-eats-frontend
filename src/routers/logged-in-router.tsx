1c47cf891857b9f268652edcb78e0103526ecb5b
Kyungmi Koong <kyungmi.k@woowahan.com>
2021-04-22 16:36:25 +0900

    15.15 Using the Token

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,14 +1,40 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
 import React, { FC } from 'react';
-import { isLoggedInVar } from '../apollo';
+import { authTokenVar, isLoggedInVar } from '../apollo';
+import { MeQuery } from '../__generated__/MeQuery';
+
+const ME_QUERY = gql`
+  query MeQuery {
+    me {
+      id
+      email
+      role
+      verified
+    }
+  }
+`;
 
 export const LoggedInRouter: FC = () => {
-  const onClick = () => {
+  const { data, loading, error } = useQuery<MeQuery>(ME_QUERY);
+
+  if (loading || error || !data) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <span className="font-medium text-xl tracking-wide">Loading...</span>
+      </div>
+    );
+  }
+
+  const onLogout = () => {
     isLoggedInVar(false);
+    authTokenVar(null);
   };
+
   return (
     <div>
-      <h1>Logged In</h1>
-      <button onClick={onClick}>Log Out</button>
+      <h1>{data.me.email}</h1>
+      <button onClick={onLogout}>Log Out</button>
     </div>
   );
 };
